37def238c3b7e2856dba9c65b6a6d4e282eb25fd
QUazAR9999 <ystep.gd.351@gmail.com>
2024-07-20 15:47:01 -0700

    Created window with SDL

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "includes.h"
 int main(int argc, char** argv[]) {
+	const int width = 800, height = 600;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("UwU you've been pwn'd by a gay, a furry, and a hacker", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_ALLOW_HIGHDPI);
+	if (window == NULL) {
+		std::cout <<  "Window failure caused by: " << SDL_GetError() << std::endl;
+	}
 	std::cout << "?";
 	return 0;
 }
